4846749841595f0e32b338a57b901fd9f31018ac
alexrsipn <aruiz1501@alumno.ipn.mx>
2023-01-26 18:26:52 -0600

    ADD hero, projects section, ADV skills, darkMode, response

---
+++ components/hero.tsx
@@ -0,0 +1,45 @@
+import Image from "next/image";
+
+const Hero = (): JSX.Element => {
+  return (
+    <section
+      id="Home"
+      className="w-full bg-aliceBlue-500 dark:bg-rbFogra29-500 bg-hero-pattern"
+    >
+      <div className=" flex flex-col lg:flex lg:flex-row m-8">
+        <div className="w-full lg:w-2/3 flex flex-col justify-center items-center">
+          <div className="w-full text-center lg:text-left lg:px-16">
+            <h2 className="font-black text-2xl leading-relaxed lg:text-5xl lg:leading-loose text-cornflowerBlue-700 dark:text-aliceBlue-400">
+              Hola, soy Alexis Ruiz 👋
+            </h2>
+            <h4 className="font-semibold text-xl lg:text-3xl leading-relaxed text-cornflowerBlue-600 dark:text-aliceBlue-500">
+              Desarrollador Analista
+            </h4>
+            <p className="font-normal leading-normal text-cornflowerBlue-600 bg-white bg-opacity-80 lg:bg-transparent rounded text-center lg:text-left dark:text-aliceBlue-500">
+              Ayudo a compañías a desarrollar soluciones
+            </p>
+          </div>
+        </div>
+        <div className="lg:w-1/3 flex justify-center lg:justify-start items-center">
+          <Image
+            src="/profilePhoto.png"
+            alt="Profile illustration hero"
+            width="388"
+            height="434"
+            className="w-4/5 lg:w-full"
+          />
+        </div>
+      </div>
+      <div className="w-full flex justify-center items-center mb-8">
+        <a
+          href="mailto:alexrs.escom@gmail.com"
+          className="font-semibold text-lg lg:text-2xl text-white bg-emerald-500 hover:bg-emerald-600 dark:bg-emerald-600 dark:hover:bg-emerald-700 px-16 lg:px-32 py-4 rounded-md shadow-md"
+        >
+          ¡Hablemos!
+        </a>
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
---
+++ components/projects.tsx
@@ -0,0 +1,93 @@
+import Image from "next/image";
+import Link from "next/link";
+
+const Projects = (): JSX.Element => {
+  return (
+    <section
+      id="Projects"
+      className="container bg-aliceBlue-200 dark:bg-rbFogra29-500"
+    >
+      <div className="m-8">
+        <h2 className="font-bold text-2xl lg:text-4xl leading-loose">Proyectos</h2>
+        <div className="flex flex-col justify-center items-center gap-4">
+          {/* Proyecto INCOM */}
+          <div className="w-4/5 h-96 lg:h-64 grid sm:grid-flow-row lg:grid-flow-col lg:auto-cols-fr sm:auto-rows-fr border border-rbFogra29-500 rounded-md shadow-md hover:scale-[1.01] hover:shadow-2xl transition ease-out duration-500">
+            <div className="flex flex-col justify-center items-center gap-4 order-1">
+              <div className="text-center">
+                <h4 className="font-semibold text-lg lg:text-2xl tracking-widest leading-relaxed">
+                  INCOM
+                </h4>
+                <p className="font-medium text-sm lg:text-base px-4 leading-normal max-w-lg">
+                  Empresa mexicana dedicada a la comercialización de insumos
+                  para telecomunicaciones a las principales empresas líder del
+                  sector
+                </p>
+              </div>
+              <Link
+                href="#"
+                className="px-6 py-2 bg-indigo-600 hover:bg-indigo-700 rounded-md text-white font-mono font-semibold"
+              >
+                Caso de estudio
+              </Link>
+            </div>
+            <a
+              href="https://www.incom.mx"
+              title="INCOM - La ferretera de las telecomunicaciones"
+              target="_blank"
+              rel="noreferer noopener"
+              className="relative order-2"
+            >
+              <Image
+                src="/newIncom.png"
+                alt="Proyecto INCOM"
+                width="600"
+                height="444"
+                className="absolute inset-0 h-full w-full object-cover object-top opacity-50 hover:opacity-100 transition ease-in-out duration-500 lg:rounded-tr-md lg:rounded-br-md"
+              ></Image>
+            </a>
+          </div>
+          {/* Fin de proyecto INCOM */}
+          {/* Proyecto MAUHAUS */}
+          <div className="w-4/5 h-96 lg:h-64 grid sm:grid-flow-row lg:grid-flow-col lg:auto-cols-fr sm:auto-rows-fr border border-rbFogra29-500 rounded-md shadow-md hover:scale-[1.01] hover:shadow-2xl transition ease-out duration-500">
+            <div className="flex flex-col justify-center items-center gap-4 order-1 lg:order-2">
+              <div className="text-center">
+                <h4 className="font-semibold text-lg lg:text-2xl tracking-widest leading-relaxed">
+                  MAUHAUS TOYS
+                </h4>
+                <p className="font-medium text-sm lg:text-base px-4 leading-normal max-w-lg">
+                  Empresa mexicana especializada en venta de coleccionables como
+                  Funko, NECA, McFarlane, IronStudios, MarvelSelect, Hot Toys,
+                  Disney y más.
+                </p>
+              </div>
+              <Link
+                href="#"
+                className="px-4 py-2 bg-indigo-600 hover:bg-indigo-700 rounded-md text-white font-mono font-semibold"
+              >
+                Caso de estudio
+              </Link>
+            </div>
+            <a
+              href="https://www.mauhaustoys.com"
+              title="MAUHAUSTOYS - Del mito a la realidad"
+              target="_blank"
+              rel="noreferer noopener"
+              className="relative order-1"
+            >
+              <Image
+                src="/newMauhaus.png"
+                alt="Proyecto MAUHAUS"
+                width="600"
+                height="444"
+                className="absolute inset-0 h-full w-full object-cover object-top opacity-50 hover:opacity-100 transition ease-in-out duration-500 lg:rounded-tr-md lg:rounded-br-md"
+              ></Image>
+            </a>
+          </div>
+          {/* Fin de proyecto MAUHAUS */}
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Projects;
